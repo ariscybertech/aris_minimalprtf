57fa5b063b081d89da4058b1a4ba8de53b620c4c
ariscybertech <ariscybertech@gmail.com>
2022-01-06 15:02:14 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Minimal Portfolio App (Flutter)
+
+## Project Goal
+
+This is a minimal portfolio/contact app created while learning Flutter. It was created along side of the londonappbrewery Flutter course on Udemy. https://github.com/londonappbrewery
---
+++ lib/main.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey.shade600,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/profile_pic.jpg'),
+              ),
+              Text(
+                'Connor Loftus',
+                style: TextStyle(
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Pacifico',
+                ),
+              ),
+              Text(
+                'MOBILE DEVELOPER',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'SourceSansPro',
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.blueGrey.shade200,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.blueGrey.shade900,
+                  ),
+                  title: Text(
+                    '(609) 672-3710',
+                    style: TextStyle(
+                      color: Colors.blueGrey.shade900,
+                      fontSize: 20.0,
+                      fontFamily: 'SourceSansPro',
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.blueGrey.shade900,
+                  ),
+                  title: Text(
+                    'connorlof@gmail.com',
+                    style: TextStyle(
+                      color: Colors.blueGrey.shade900,
+                      fontSize: 20.0,
+                      fontFamily: 'SourceSansPro',
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
